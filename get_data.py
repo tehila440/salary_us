b7987e9c12d8470ddfa46ce5157ba5c63ca3c138
tehila440 <tehila.crowell@gmail.com>
2020-12-28 19:26:50 +0200

    code update

--- get_data.py
+++ get_data.py
@@ -4,6 +4,6 @@ import pandas as pd
 path = '/Users/tehila/PycharmProjects/salary_us/chromedriver'
 
 
-df = gs.get_jobs('engineer',15, False, path, 15)
-#df.to_csv('glassdoor_jobs.csv', index = False)
+df = gs.get_jobs('data scientist',1000, False, path, 15)
+df.to_csv('glassdoor_jobs_us.csv', index = False)
 
